71be3bca90af2b98e39e8e129d96117d438b579f
Junior99999 <juniormachdo11@gmail.com>
2021-05-24 17:29:20 -0300

    Novo

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports ={
+    dialect:'postgres',
+    hots:'localhost',
+    port: '5432',
+    username: 'postgres',
+    password: '1234',
+    database: 'esportes',
+    define:{
+        timestemps:true
+    }
+}
