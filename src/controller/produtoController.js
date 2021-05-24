4252f40e32d72aad262a5f13a7ae8687d6219327
Junior99999 <juniormachdo11@gmail.com>
2021-05-24 20:01:50 -0300

    Aula 8

--- src/controller/produtoController.js
+++ src/controller/produtoController.js
@@ -1,11 +1,22 @@
 const Produto = require('../model/Produto')
 
+const { Op } = require('sequelize')
+ 
 module.exports = {
     async list(req,res){
-
+        const produtos = await Produto.findAll()
+        return res.render('admin/produto/list.ejs', {'Produtos':produtos, 'msg': req.flash('msg')})
     },
     async filtro(req,res){
-
+        let query = '%'+req.body.filtro+'%'
+        const produtos = await Produto.findAll({
+            where:{
+                nome: {
+                    [Op.like]: query
+                }
+            }
+        })
+        return res.render('admin/produto/list.ejs', {'Produtos':produtos, 'msg': req.flash('msg')})
     },
     async abreadd(req,res){
         res.render('admin/produto/add.ejs',{ msg : req.flash('msg') })
@@ -22,12 +33,44 @@ module.exports = {
         });
     },
     async abreedit(req,res){
-
+        const id = req.params.id;
+        const produto = await Produto.findByPk(id);
+        console.log(produto)
+        return res.render('admin/produto/edit.ejs', {'Produto':produto, 'msg': req.flash('msg')})
     },
     async edit(req,res){
-
+        const id = req.params.id;
+        const produto = await Produto.findByPk(id)
+        produto.nome = req.body.nome;
+        produto.valor = req.body.valor;
+        produto.tipo = req.body.tipo;
+        produto.save().then(
+            (produto) =>{
+                req.flash('msg',produto.nome + ' foi editado com sucesso!');
+                res.render('admin/produto/edit.ejs',{'Produto':produto, 'msg': req.flash('msg')})
+            },
+            (err) =>{
+                req.flash('msg','Problema ao alterar produto');
+                res.render('admin/produto/edit.ejs',{'Produto':produto, 'msg': req.flash('msg')})
+            }
+        );
     },
     async del(req,res){
+        const id = req.params.id;
+        await Produto.destroy({
+            where: {
+                id:id
+            }
+        }).then(
+            (produto) => {
+                req.flash('msg','Produto foi editado com sucesso!');
+                res.redirect('/admin/produto/')
+            },
+            (err) => {
+                req.flash('msg','Problema ao deletar produto');
+                res.redirect('/admin/produto/')
+            }
 
+        )
     },
 }
--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ var session = require('express-session')
 require('./database/index')
 
 app.use(session({secret:'123', resave:true,saveUninitialized:true}))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(flash())
 
 app.set('view engine', 'ejs')
-app.set('views', 'src/views')
+app.set('views', __dirname+'/views')
 
 app.use(express.static(path.join("src", "public")))
 
